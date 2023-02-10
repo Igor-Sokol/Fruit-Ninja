0f723e3dc223385f1e48f7d92f5d2ab402c7c158
Igor Sokol <igor.sokol.isa@gmail.com>
2023-02-10 17:10:37 +0300

    Added a component to update the status of the playing field services.

---
+++ Assets/Scripts/PlayingFieldComponents/PlayingFieldUpdate.cs
@@ -0,0 +1,18 @@
+using PlayingFieldServices;
+using UnityEngine;
+
+namespace PlayingFieldComponents
+{
+    public class PlayingFieldUpdate : MonoBehaviour
+    {
+        [SerializeField] private BlockContainer playingField;
+
+        private void Update()
+        {
+            foreach (IOnPlayingField block in playingField.Blocks)
+            {
+                block.OnPlayingField();
+            }
+        }
+    }
+}
