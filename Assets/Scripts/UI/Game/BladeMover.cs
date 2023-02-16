7d7b4dddd6f3c341ec9b994b34d8b521c0d7d3bd
Igor Sokol <igor.sokol.isa@gmail.com>
2023-02-16 17:24:33 +0300

    Blade movements can now be interrupted

--- Assets/Scripts/UI/Game/BladeMover.cs
+++ Assets/Scripts/UI/Game/BladeMover.cs
@@ -4,22 +4,34 @@ using UnityEngine.EventSystems;
 
 namespace UI.Game
 {
-    public class BladeMover : MonoBehaviour, IDragHandler, IBeginDragHandler
+    public class BladeMover : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
     {
+        private bool _dragging;
+        
         [SerializeField] private Blade blade;
         [SerializeField] private Camera workingCamera;
     
         public bool Active { get => gameObject.activeSelf; set => gameObject.SetActive(value); }
-        
-        public void OnDrag(PointerEventData eventData)
-        {
-            blade.transform.position = (Vector2)workingCamera.ScreenToWorldPoint(eventData.position);
-        }
 
         public void OnBeginDrag(PointerEventData eventData)
         {
             blade.transform.position = (Vector2)workingCamera.ScreenToWorldPoint(eventData.position);
             blade.ClearTrail();
+            
+            _dragging = true;
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            if (_dragging)
+            {
+                blade.transform.position = (Vector2)workingCamera.ScreenToWorldPoint(eventData.position);
+            }
+        }
+        
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            _dragging = false;
         }
     }
 }
