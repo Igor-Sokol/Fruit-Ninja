4a9baf39b3346fdea50acb2b63280bddcf19e84a
Igor Sokol <igor.sokol.isa@gmail.com>
2023-02-20 20:54:20 +0300

    TextValue changed to use string.Format

--- Assets/Scripts/UI/TextValue.cs
+++ Assets/Scripts/UI/TextValue.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using TMPro;
 using UnityEngine;
@@ -6,14 +7,13 @@ namespace UI
 {
     public class TextValue : MonoBehaviour
     {
+        private string _textMask;
         private float _timer;
         private float _previousValue;
         private Coroutine _coroutineHandler;
         private int _value;
         
         [SerializeField] private TMP_Text tmpText;
-        [SerializeField] private string text;
-        [SerializeField] private string endText;
         [SerializeField] private float valueChangeTime;
 
         public void SetValue(int score)
@@ -31,7 +31,9 @@ namespace UI
 
         private void ChangeText(int score)
         {
-            tmpText.text = text + score + endText;
+            _textMask ??= tmpText.text;
+
+            tmpText.text = string.Format(_textMask, score);
         }
         
         private IEnumerator ValueChanger()
